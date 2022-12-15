9b1d000a9ad72d36ff41c580ff18ad1b01b6f334
Benmerabet Ayoub <62332376+ay19b@users.noreply.github.com>
2022-12-15 20:16:26 +0100

    reducer

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -7,7 +7,10 @@ export const productSlice = createSlice({
   
   reducers: {
     add: (state, action) => {
-        state.push({...action.payload, quantity: 1,added: true});
+      const itemInCart = state.find((item) => item.id === action.payload.id);
+      if (!itemInCart) {
+        state.push({ ...action.payload, quantity: 1,added: true});
+      } 
     },
     remove: (state, action) => {  
       state.splice(state.findIndex((item) => item.id === action.payload.id),1)
